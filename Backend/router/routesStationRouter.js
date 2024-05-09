6bff2a2c52d7d5bea20b5b8721af0ce6bfd5b636
Sujal_Dhiman <116187046+SujalDhiman@users.noreply.github.com>
2024-05-09 19:54:30 +0530

    Delete Backend/router directory

--- Backend/router/routesStationRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { getRoutes , insertRoute} from "../controllers/routes.controller.js"
-const router=express.Router()
-
-
-router.route("/getRoute").get(getRoutes)
-router.route("/insertRoute").post(insertRoute)
-
-export default router
